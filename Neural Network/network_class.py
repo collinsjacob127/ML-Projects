072cbe9086970c18c478f7cfb6a1e6db8dc115c7
Jacob Collins <collinsjacob127@gmail.com>
2021-12-31 14:34:02 -0800

    updating network program and adding basic functionality

--- Neural Network/network_class.py
+++ Neural Network/network_class.py
@@ -1,2 +1,41 @@
 # Neural Network class definition
-#
+# Jacob Collins
+# 31 December 2021
+import numpy as np
+
+# Class def
+class Network:
+    # Class initializer
+    # Initializes the weights
+    # @param array structure - number of nodes for each layer (0 - input, max - output)
+    def __init__(self, structure):
+        # Process structure and initialize weights
+        # self.weights - list of each weight matrix in sequential order.
+        self.weights = list()
+        for i in range(len(structure)-1):
+            self.weights.append(0.02*np.random.random((structure[i],structure[i+1]))-0.01)
+
+
+
+    # Forward propagation
+    # Runs input through the network and gives output
+    def forward_prop(self, input):
+        # Connect input to first hidden layer and output
+        pass
+    # Backpropagation
+    # Compares output to correct response and adjusts weights accordingly
+    def back_prop(self, output, correct):
+        # Check alignment of output and correct
+        pass
+
+    # Iterative Learning
+    # Automatically runs through batches and trains the network;
+    # Saves output to text file
+    # @param iterations - number of iterations to run through
+    # @param inputs - list of inputs for running through
+    # @param corrects - list of correct responses for comparison
+    def iterative_learning(self, iterations, inputs, corrects):
+        pass
+
+net = Network([5, 4, 3, 2])
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ML-Projects
 Private backup of my ML code and projects
+
+# Plans going forward:
+- Convolutional Network class
+- **Network Visualizer**
+- Weight saving process (text file?)
+- Modular activation functions and backpropagation methods
+- Modular NLP
+- 
